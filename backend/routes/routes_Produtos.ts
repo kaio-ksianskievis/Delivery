7a022dbbbd6d1044d05fe08b293fce6a10829048
kaio <ksianskieviskaio@gmai.com>
2024-11-28 19:57:43 -0300

    rota dos Produtos

---
+++ backend/routes/routes_Produtos.ts
@@ -0,0 +1,39 @@
+import App from "../src";
+import { get_Produtos,get_Produtos_Nome,create_Produtos,delete_Produtos,update_Produtos } from "../services/services_Produtos";
+
+//rotas get
+
+App.get("/Produtos",(req,res)=>{
+
+    get_Produtos(req,res)
+})
+
+App.get("/Produtos/:Nome",(req,res)=>{
+
+    get_Produtos_Nome(req,res)
+    
+})
+
+//rota post
+
+App.post("/Produtos",(req,res)=>{
+    
+    create_Produtos(req,res)
+
+})
+
+//rota put
+
+App.put("/Produtos/:id",(req,res)=>{
+
+    update_Produtos(req,res)
+    
+})
+
+//rota delete
+
+App.delete("/Produtos/:id",(req,res)=>{
+    
+    delete_Produtos(req,res)
+
+})
