08c4483567dff80984c012a11f9c204aa8efe0f8
kaio <ksianskieviskaio@gmai.com>
2024-11-28 19:16:51 -0300

    adicioando service de Avaliações

---
+++ backend/services/services_Avaliações.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import Avaliações from "../models/Avaliações"
+
+//busca por todos os Avaliações
+
+async function get_Produtos(req:Request,res:Response){
+    try{
+        const obj = await Avaliações.findAll({raw: true})
+        res.json(obj)
+        res.status(200)
+    }catch{
+        res.status(500)
+        
+    }
+}
+//busca por uma Avaliação com o parametro Id_Restaurante
+
+async function get_Produtos_Nome(req:Request,res:Response) {
+    try{
+        const Id_Restaurante = req.params
+        const obj = await Avaliações.findOne({where:{Id_Restaurante:Id_Restaurante}})
+        res.json(obj)
+        res.status(200)
+    }catch{
+        res.status(404)
+    }
+}
+
+
+
+// criando Avaliação
+
+async function create_Produtos(req:Request,res:Response) {
+    try{
+        const obj = req.body
+        await Avaliações.create(obj)
+        res.status(200)
+    }catch{
+        res.status(400)
+    }
+}
+
+// deletando Avaliações
+
+async function delete_Produtos(req:Request,res:Response) {
+    try{
+        const id = req.params
+        await Avaliações.destroy({where: {id: id}})
+        res.status(200)
+    }catch{
+        res.status(404)
+    }
+}
