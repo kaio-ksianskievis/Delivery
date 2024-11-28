f3cef8e74f35a855ff6445a1e965ed4f559c7a4f
kaio <ksianskieviskaio@gmai.com>
2024-11-28 19:12:37 -0300

    adicioando service de Produtos

---
+++ backend/services/services_Produtos.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import Produtos from "../models/Produtos";
+
+
+//busca por todos os Produtos
+
+async function get_Produtos(req:Request,res:Response){
+    try{
+        const obj = await Produtos.findAll({raw: true})
+        res.json(obj)
+        res.status(200)
+    }catch{
+        res.status(500)
+        
+    }
+}
+//busca por um Produto com o parametro nome
+
+async function get_Produtos_Nome(req:Request,res:Response) {
+    try{
+        const name = req.params
+        const obj = await Produtos.findOne({where:{Nome:name}})
+        res.json(obj)
+        res.status(200)
+    }catch{
+        res.status(404)
+    }
+}
+
+// autalizando Produtos
+
+async function update_Produtos(req:Request,res:Response) {
+    try{
+        const id = req.params
+        const {Nome,Descrição,Id_Restaurante} = req.body
+        await Produtos.update({Nome,Descrição,Id_Restaurante},{where: {id: id}})
+        res.status(200)
+    }catch{
+        res.status(400)
+    }
+}
+
+// criando Produtos
+
+async function create_Produtos(req:Request,res:Response) {
+    try{
+        const obj = req.body
+        await Produtos.create(obj)
+        res.status(200)
+    }catch{
+        res.status(400)
+    }
+}
+
+// deletando Produtos
+
+async function delete_Produtos(req:Request,res:Response) {
+    try{
+        const id = req.params
+        await Produtos.destroy({where: {id: id}})
+        res.status(200)
+    }catch{
+        res.status(404)
+    }
+}
