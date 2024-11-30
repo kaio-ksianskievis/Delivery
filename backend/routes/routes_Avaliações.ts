5067c812638606edcc1707caddec0521f4eea7a6
kaio <ksianskieviskaio@gmai.com>
2024-11-30 15:09:26 -0300

    atualizando services de Avaliações

--- backend/routes/routes_Avaliações.ts
+++ backend/routes/routes_Avaliações.ts
@@ -1,9 +1,9 @@
-import { get_Avaliação_Nome,create_Avaliação,delete_Avaliação } from "../services/services_Avaliações";
+import { get_Avaliação_Nome,create_Avaliação} from "../services/services_Avaliações";
 import { router } from "../src";
 
 //rotas get
 
-router.get("/Restaurante/:nome/Avaliações",(req,res)=>{
+router.get("/Restaurantes/:Nome/Avaliações",(req,res)=>{
 
     get_Avaliação_Nome(req,res)
 })
@@ -11,16 +11,8 @@ router.get("/Restaurante/:nome/Avaliações",(req,res)=>{
 
 //rota post
 
-router.post("/Avaliação",(req,res)=>{
+router.post("/Restaurantes/:Nome/Avaliações",(req,res)=>{
     
     create_Avaliação(req,res)
-
-})
-
-//rota delete
-
-router.delete("/Avaliação/:id",(req,res)=>{
-    
-    delete_Avaliação(req,res)
 
 })
--- backend/services/services_Avaliações.ts
+++ backend/services/services_Avaliações.ts
@@ -17,28 +17,17 @@ async function get_Avaliação_Nome(req:Request,res:Response) {
 
 async function create_Avaliação(req:Request,res:Response) {
     try{
-        const obj = req.body
-        await Avaliações.create(obj)
+        const Nome = req.params.Nome
+        const {Descrição,Estrelas} = req.body
+
+        await Avaliações.create({Descrição: Descrição, Nome: Nome, Estrelas:Estrelas})
         res.status(200)
     }catch{
         res.status(400).json({"Erro":"Não foi possivel criar a Avaliação"})
     }
 }
 
-// deletando Avaliações
-
-async function delete_Avaliação(req:Request,res:Response) {
-    try{
-        const id = req.params.id
-        await Avaliações.destroy({where: {id: id}})
-        res.status(200)
-    }catch{
-        res.status(404).json({"Erro":"Não foi possivel deletar a Avaliação"})
-    }
-}
-
 export {
     get_Avaliação_Nome,
-    delete_Avaliação,
     create_Avaliação
 }
