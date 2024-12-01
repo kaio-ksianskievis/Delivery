74c0415c44bce15c57ee4568d8764a572c80c3b2
kaio <ksianskieviskaio@gmai.com>
2024-11-30 22:48:03 -0300

    adicionando middlewares

--- backend/authentication/auth.ts
+++ backend/authentication/auth.ts
@@ -1,10 +1,11 @@
 import { Request,Response,NextFunction } from "express";
 import User from "../models/User";
 import { compare } from "bcrypt";
+import { sign,verify } from "jsonwebtoken";
 
 //criando midware para verificação de login
 
-export async function busca_login(req:Request,res:Response,next:NextFunction) {
+async function busca_login(req:Request,res:Response,next:NextFunction) {
     
     const {Email,Password,Função} = req.body
     const data = await User.findOne({where:{Email:Email}})
@@ -25,3 +26,55 @@ export async function busca_login(req:Request,res:Response,next:NextFunction) {
 
     }
 }
+
+//gera token
+
+async function gera_toke(req:Request,res:Response) {
+    
+    const {Email,Password,Função} = req.body
+    const data = await User.findOne({where:{Email:Email}})
+    if(data != null && data != undefined){
+
+        compare(Password,data.dataValues.Password,(err,obj)=>{
+
+            if(obj){
+                sign({id:data.dataValues.id,Email:Email},process.env.SECRET,{expiresIn:"5h"},(err,token)=>{
+                    if(token){
+                        res.status(200).json({"token":token})
+                    }else{
+                        res.status(500).json({"erro":"falha interna"})
+                    }
+                })
+            }else{
+                res.status(400).redirect("/login")
+            }
+        })
+    }else{
+
+        res.status(400).redirect("/login")
+
+    }
+
+}
+
+// middleware que  confere token
+
+async function auth_token(req:Request,res:Response,next:NextFunction) {
+    var token = req.headers.authorization
+    if(token != undefined && token != null){
+        const bearer = token.split(" ")
+        token = bearer[1]
+        verify(token,process.env.SECRET,(err,data)=>{
+            if(data){
+                next()
+            }else{
+                res.status(400).send("token inválido")
+            }
+        })
+    }else{
+        
+        res.status(400).send("token inválido")
+    }
+}
+
+export {gera_toke,busca_login,auth_token}
